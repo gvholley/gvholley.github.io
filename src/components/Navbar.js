6e578939f7b996e1a8f6ac442160dfafb365dd5c
Grant Holley <gvholley@gmail.com>
2020-12-14 21:10:31 +0900

    Setting up navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+
+function Navbar() {
+  return (
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          GVH
+        </Link>
+
+        </div>
+      </nav>
+
+    </React.Fragment>
+    )
+}
+
+
