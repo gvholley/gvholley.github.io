dc66fb2409f74ad37efe0e6e4d119d83b3207bc7
Grant Holley <gvholley@gmail.com>
2020-12-14 22:49:27 +0900

    Edited navbar elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
   return (
     <React.Fragment>
       <nav className="navbar">
@@ -9,7 +15,31 @@ function Navbar() {
         <Link to="/" className="navbar-logo">
           GVH
         </Link>
-
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul classname={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Landing Pages
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
 
@@ -18,3 +48,4 @@ function Navbar() {
 }
 
 
+export default Navbar;
